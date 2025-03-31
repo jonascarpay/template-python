d3eb4d3c34a70d2581267d7b44815dd83b20ea08
Jonas Carpay <jonascarpay@gmail.com>
2025-03-31 13:57:37 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+  inputs.mkflake.url = "github:jonascarpay/mkflake";
+
+  outputs = { nixpkgs, mkflake, self }: mkflake.lib.mkflake {
+    perSystem = system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        myPython = pkgs.python3.withPackages (p: [
+          # Packages go here
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            myPython
+            pkgs.ruff
+            pkgs.pyright
+            pkgs.black
+            pkgs.ruff-lsp
+          ];
+        };
+      };
+  };
+}
