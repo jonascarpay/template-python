c869d1f5a7faf94c6f3a09b712d476bbe5e58526
Jonas Carpay <jonascarpay@gmail.com>
2025-05-08 12:23:24 +0900

    template-python -> templates

--- flake.nix
+++ flake.nix
@@ -1,18 +1,46 @@
 {
 
-  description = "Nix + Python project templates";
+  description = "Nix project templates";
 
   outputs = { self }: {
     templates = {
-      default = self.templates.vanilla;
-      uv = {
-        description = "UV Python project template";
-        path = ./uv;
+
+      python-uv = {
+        path = ./python-uv;
+        description = "uv, with nix providing uv, pyright, ruff and the python interpreter";
+        welcomeText = ''
+          # Getting started
+          To initialize the project, enter the dev shell, and run `uv init`.
+
+          # Configuration
+          After initializing, the "ruff.toml" and "pyrightconfig.json" files can optionally be combined with "pyproject.toml".
+
+          ## pyright
+          ```
+          [tool.pyright]
+          strict = true
+          venvPath = "."
+          venv = ".venv"
+          ```
+
+          ## ruff
+          ```
+          [tool.ruff.lint]
+          target-version = "py312"
+          line-length = 160
+          select = [ "ALL" ]
+          ignore = [
+            # disable as necessary
+          ]
+          ```
+        '';
       };
-      vanilla = {
-        description = "Pure Nix Python project template";
-        path = ./vanilla;
+
+      python-vanilla = {
+        description = "pure nix python project template";
+        path = ./python-vanilla;
       };
+
     };
   };
 }
